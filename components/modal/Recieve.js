09c9fe41282839c514cb887a546fbbf97656698d
Yadvendra2998 <yadvendra2998@gmail.com>
2022-03-17 21:36:17 +0530

    complete

--- components/modal/Recieve.js
+++ components/modal/Recieve.js
@@ -37,7 +37,7 @@ const Recieve = ({setAction, selectedToken, walletAddress}) => {
               <Address>{walletAddress}</Address>
             </div>
             <CopyButton onClick={() => {
-              navigator.clipboard.writText(walletAddress)
+              navigator.clipboard.writeText(walletAddress)
               setCopied(true)
             }}>
               {copied ? <FaCheck style={{color:'#27ad75'}} /> : <BiCopy/>}
